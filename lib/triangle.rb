f0bdd2f33abed7a585ae616f6119c61a1563fff3
Hanna Lee <mani21na@gmail.com>
2019-09-05 21:23:27 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,21 @@
 class Triangle
-  # write code here
+  attr_accessor :side1, :side2, :side3
+  
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+    check
+  end
+  
+  def check
+    new_tri = ""
+    if @side1 == @side2 && @side2 == @side3
+      new_tri = :equilateral
+    end
+  end  
+  
+  class TriangleError < StandardError
+    
+  end  
 end
